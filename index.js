806c8383467d678eb3b33efcbc1e6afef1fa7814
roshad <roshad87a9@gmail.com>
2022-11-04 18:20:01 +0800

    settimeout

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const notifier = require("node-notifier");
 
 // start time
+let interval = 1/60;
+const repeat = () => {
+  notifier.notify({
+    message:  new Date() + "",
+    sound: true,
+    actions:[1, 10,30],
+  },  function (error, response, metadata) {
+    interval=new Number(response)!=NaN?response:1/6;
+   console.log("set interval",interval,"min");
+    setTimeout(repeat, interval*60*1000);
+  });
 
-setInterval(() => {
-    notifier.notify({
-      message:  new Date() + "",
-      sound: true,
-    });
-}, 1000*60);
+}
+setTimeout(repeat, interval*60*1000);
